5f50124c348c14cc3d9cb013ac0885fa7626445c
Vivianpcamargo <vivianpaulacamargo@gmail.com>
2023-11-01 21:39:28 -0300

    ✨ aula 6

---
+++ Aulas/aula_06/src/livro.py
@@ -0,0 +1,6 @@
+class Livro():
+    def __init__(self, id, titulo, preco, data_publicacao):
+        self.id = id
+        self.titulo = titulo
+        self.preco = preco
+        self.data_publicacao = data_publicacao
